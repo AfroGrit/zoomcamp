3f3100c25daec88dfac7743e50d22379b0e2fb21
AfroGrit <jlkitunka@gmail.com>
2022-10-23 17:13:12 +0200

    chore: updated doc string for the bento service

--- ML/module-7/_hw7_service.py
+++ ML/module-7/_hw7_service.py
@@ -1,15 +1,21 @@
 import bentoml
-from bentoml.io import JSON
+from bentoml.io import JSON, NumpyNdarray
 
-model_ref = bentoml.sklearn.get('mlzoomcamp_homework:qtzdz3slg6mwwdu5')
-# model_ref = bentoml.sklearn.get('mlzoomcamp_homework:jsi67fslz6txydu5')
+# model_ref = bentoml.sklearn.get('mlzoomcamp_homework:qtzdz3slg6mwwdu5')
+model_ref = bentoml.sklearn.get('mlzoomcamp_homework:jsi67fslz6txydu5')
 model_runner = model_ref.to_runner()
 svc = bentoml.Service('coolmodel', runners=[model_runner])
 
-@svc.api(input=JSON(), output=JSON())
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
 def classify(application_data):
   """
-  This function generates the endpoint to serve our model.
+  description: Model service: classifier
+  This function generates predictions from API calls.
+  parameters:
+  - data: User data
+    description: User data
+    required: true
+    type: NumpyNdarray    
   """
   # prediction = model_runner.predict.run(application_data)
   return model_runner.predict.run(application_data)
